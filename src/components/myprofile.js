1207bfc7fbc8e624e3ae249ea17814770a41bcd0
Alex Ogeto <103739438+Osoro254Alex@users.noreply.github.com>
2022-12-08 23:22:36 +0300

    Dragons progress

--- src/components/myprofile.js
+++ src/components/myprofile.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styles from './myprofile.module.css';
 
 const Myprofile = () => {
-  // const rocketsData = useSelector((state) => state.rockets);
   // const missionsData = useSelector((state) => state.missions);
   const dragonsData = useSelector((state) => state.dragons);
   return (
-    <div className="profile-container">
-
+    <div className={styles.profile_container}>
       {/* <div className="reserved-rockets">
         <h2>My Missions</h2>
         <ul className="rockets-list">
@@ -18,20 +17,13 @@ const Myprofile = () => {
         </ul>
       </div> */}
 
-      {/* <div className="reserved-rockets">
-        <h2>My Rockets</h2>
-        <ul className="rockets-list">
-          {rocketsData.map((rocket) => {
-            if (rocket.reserved) { return <li key={rocket.id}>{rocket.rocket_name}</li>; }
-            return '';
-          })}
-        </ul>
-      </div>  */}
-      <div className="reserved-dragons">
+      <div className={styles.reserved_dragons}>
         <h2>My Dragons</h2>
-        <ul className="dragons-list">
+        <ul className={styles.dragons_list}>
           {dragonsData.map((dragon) => {
-            if (dragon.reserved) { return <li key={dragon.id}>{dragon.name}</li>; }
+            if (dragon.reserved) {
+              return <li key={dragon.id}>{dragon.name}</li>;
+            }
             return '';
           })}
         </ul>
