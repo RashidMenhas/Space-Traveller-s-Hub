4c5f1eb411bf1bc61ddf4e0f346a05a2b76a6431
RashidMenhas <rashidmenhas10@gmail.com>
2022-12-09 16:10:51 +0500

    import useSelector MyProfile render the list data where clicked on the Reserved button it will hold the data. it will only hold the rocket name.

--- src/components/myprofile.js
+++ src/components/myprofile.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './myprofile.css';
 
-const Myprofile = () => (
-  <div>
-    <h1>My profile</h1>
-  </div>
-);
+const Myprofile = () => {
+  const rocketsData = useSelector((state) => state.rockets);
+  return (
+    <div className="profile-container">
+      <div>
+        <h2>My Rockets</h2>
+        <ul className="table1">
+          {rocketsData.map((rocket) => {
+            console.log(rocketsData);
+            if (rocket.reserved) {
+              return <li className="data" key={rocket.id}>{rocket.name}</li>;
+            }
+            return '';
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Myprofile;
