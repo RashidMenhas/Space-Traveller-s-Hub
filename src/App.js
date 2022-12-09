d807ae310be1062a245458a7e137b25c1dec487f
Juwon <dnladesanya1@gmail.com>
2022-12-09 07:41:55 +0100

    edited router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/nevbar';
+import Missions from './components/missions';
 
 const App = () => (
-  <div>
+  <>
     <Navbar />
-  </div>
+    <Routes>
+      <Route path="/missions" element={<Missions />} />
+    </Routes>
+  </>
 );
 export default App;
