c9e7a7965b14663d62301795e7c0453bdbc242a8
RashidMenhas <rashidmenhas10@gmail.com>
2022-12-09 16:30:55 +0500

    basic design of index file

--- src/rocket-api/rocket-api.js
+++
@@ -1,9 +0,0 @@
-const baseurl = 'https://api.spacexdata.com/v3/rockets';
-
-const fetchdata = async () => {
-  const response = await fetch(baseurl);
-  const data = await response.json();
-  return data;
-};
-
-export default fetchdata;
