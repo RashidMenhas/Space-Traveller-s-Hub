a5337c3f486d95214809812a9a9fd07617d22298
RashidMenhas <rashidmenhas10@gmail.com>
2022-12-09 16:29:39 +0500

    import Provider and store and used inside the render function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
 
+// Add provider into main index.js file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
